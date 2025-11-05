cceedc6980835a39c0aaf07cff77480173f41a81
Shrey <shre.2002.work@gmail.com>
2025-11-05 12:28:40 +0530

    Implemented causal MHA with KV Cache

--- attention/cached_mha.py
+++ attention/cached_mha.py
@@ -24,24 +24,35 @@ class CausalSelfAttentionWithKVCache(nn.Module):
         )
     
     def forward(self, x: torch.Tensor, old_kv_cache: Optional[Tuple[torch.Tensor, torch.Tensor]] = None):
+        """
+        T_new   : The sequence length of the new prompt. Can either be length of input prompt or 1 depending on whether old_kv_cache exists or is None.
+        T_old   : The TOTAL sequence length upto the last generated token
+        T_total : T_old + T_new
+        """
         B, T, C = x.size()
-        q, k, v = self.c_attn(x).split(self.embd_size, dim=-1) # (B, T, embd_size)
-        q = q.view(B, T, self.n_heads, self.dk).transpose(1, 2) # (B, H, T, dk)
-        k = k.view(B, T, self.n_heads, self.dk).transpose(1, 2) # (B, H, T, dk)
-        v = v.view(B, T, self.n_heads, self.dk).transpose(1, 2) # (B, H, T, dk)
+        q, k, v = self.c_attn(x).split(self.embd_size, dim=-1) # (B, T_new, embd_size)
+        q = q.view(B, T, self.n_heads, self.dk).transpose(1, 2) # (B, H, T_new, dk)
+        k = k.view(B, T, self.n_heads, self.dk).transpose(1, 2) # (B, H, T_new, dk)
+        v = v.view(B, T, self.n_heads, self.dk).transpose(1, 2) # (B, H, T_new, dk)
 
         if old_kv_cache:
-            old_k, old_v = old_kv_cache
-            k = torch.cat((old_k, k), dim=1)
-            v = torch.cat((old_v, v), dim=1)
+            """
+            T_new = 1
+            q : (B, H, 1, dk)
+            """
+            old_k, old_v = old_kv_cache # (B, H, T_old, dk)
+            k = torch.cat((old_k, k), dim=2) # (B, H, T_total, dk)
+            v = torch.cat((old_v, v), dim=2) # (B, H, T_total, dk)
+            att = (q @ k.transpose(-1, -2)) / math.sqrt(self.dk) # (B, H, 1, T_total)
 
         else:
-            att = (q @ k.transpose(-1, -2)) / math.sqrt(self.dk) # (B, H, T, T)
+            att = (q @ k.transpose(-1, -2)) / math.sqrt(self.dk) # (B, H, T_new, T_new)
             att = att + self.causal_mask[:, :, :T, :T] # type: ignore[attr-defined]
-            att = F.softmax(att, dim=-1) # (B, H, T, T)
-            att = self.dropout(att) # (B, H, T, T)
-            y = att @ v # (B, H, T, dk)
-            y = y.transpose(1, 2).contiguous().view(B, T, C) # (B, T, embd_size)
 
-            y = self.dropout(self.c_proj(y)) # (B, T, embd_size)
-            return y, (k, v)
+        att = F.softmax(att, dim=-1) # (B, H, T_new, T_new)
+        att = self.dropout(att) # (B, H, T_new, T_new)
+        y = att @ v # (B, H, T_new, dk)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # (B, T_new, embd_size)
+
+        y = self.dropout(self.c_proj(y)) # (B, T_new, embd_size)
+        return y, (k, v)
